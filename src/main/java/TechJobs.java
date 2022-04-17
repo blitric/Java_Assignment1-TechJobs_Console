38c35e41fdc5c798e2b1907792d9ea8d011983af
Alma <alma.blitric@gmail.com>
2022-04-17 15:00:00 -0500

    Completed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if(someJobs.size() == 0) {
+            System.out.print("No Results");
+        } else {
+        for(HashMap<String,String> job: someJobs) {
+            System.out.println("\n*****");
+            for(Map.Entry<String, String> aJob: job.entrySet()) {
+                System.out.println(aJob.getKey() + ": " + aJob.getValue());
+            }
+            System.out.println("*****");
+        }
+        //System.out.println("printJobs is not implemented yet");
+    }
     }
 }
